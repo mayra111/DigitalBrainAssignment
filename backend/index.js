ec47651d8d9e3a54ab1717a91f8af318e74b0bc0
mayra111 <97520214+mayra111@users.noreply.github.com>
2023-08-09 14:53:05 +0530

    addede

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,14 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const GOOGLE_CUSTOM_SEARCH_API_KEY = "AIzaSyDRoHpp0ZqJjG-b7TmGQs9JuqBivu2WM_k"; // Replace with your API key
+const GOOGLE_CUSTOM_SEARCH_API_KEY = "AIzaSyDRoHpp0ZqJjG-b7TmGQs9JuqBivu2WM_k"; 
 
 app.get("/api/search", async (req, res) => {
   const { q } = req.query;
 
   try {
     const response = await axios.get(
-      //   `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_CUSTOM_SEARCH_API_KEY}&q=${q}`
+      
       `https://www.googleapis.com/customsearch/v1?key=AIzaSyDRoHpp0ZqJjG-b7TmGQs9JuqBivu2WM_k&cx=017576662512468239146:omuauf_lfve&q=${q}`
     );
     console.log(response.data);
